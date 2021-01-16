086f7d9088c40ad98200acea6d6bf052785d498c
ILumiNight <66382351+ILumiNight@users.noreply.github.com>
2021-01-16 23:58:50 +0800

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def main():
         config = json.load(f)
 
     # Make a bot
-    updater = Updater(config['token’], use_context = True)
+    updater = Updater(config['token'], use_context = True)
     
     # Load data into the bot to be accessed later on.
     updater.dispatcher.bot_data['quiz'] = config['quiz']
